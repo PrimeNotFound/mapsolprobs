feb45f325dcd2aaf8c6cd8114059c2231b23bab4
MadhavBhatt <madhavbhatt9072@gmail.com>
2025-04-03 13:24:56 +0530

    Create firtsunique.cpp

---
+++ firtsunique.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> countMap;
+        for (char c : s) {
+            countMap[c]++;
+        }  
+        for (int i = 0; i < s.length(); ++i) {
+            if (countMap[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
