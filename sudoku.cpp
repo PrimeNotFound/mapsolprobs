373e716340a27b11ae1bb7a9c82351288f0ac49f
MadhavBhatt <madhavbhatt9072@gmail.com>
2025-04-04 13:09:54 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-
-        unordered_set<string> rows[9], cols[9], subgrids[9];
+        bool isValidSudoku(vector<vector<char>>& b) {
 
         for (int i = 0; i < 9; i++) {
+            vector<int> v1(9, 1);
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;  s
-
-                char num = board[i][j];
-                string rowKey = "r" + to_string(i) + "-" + num;
-                string colKey = "c" + to_string(j) + "-" + num;
-                string subgridKey = "s" + to_string(i / 3 * 3 + j / 3) + "-" + num;
-
-                
-                if (rows[i].count(rowKey) || cols[j].count(colKey) || subgrids[i / 3 * 3 + j / 3].count(subgridKey)) {
-                    return false;
+                if (b[i][j] != '.') {
+                    int k = (b[i][j] - '0');
+                    v1[k - 1] -= 1;
+                    if (v1[k - 1] < 0)
+                        return false;
                 }
-
-                rows[i].insert(rowKey);
-                cols[j].insert(colKey);
-                subgrids[i / 3 * 3 + j / 3].insert(subgridKey);
             }
         }
+        
 
-        return true; 
+        for (int j = 0; j < 9; j++) {
+            vector<int> v2(9, 1);
+            for (int i = 0; i < 9; i++) {
+                if (b[i][j] != '.') {
+                    int k = b[i][j] - '0';
+                    v2[k - 1] -= 1;
+                    if (v2[k - 1] < 0)
+                        return false;
+                }
+            }
+        }
+        
+    
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                vector<int> v3(9, 1);
+                for (int i1 = 0; i1 < 3; i1++) {
+                    for (int j1 = 0; j1 < 3; j1++) {
+                        if (b[i + i1][j + j1] != '.') {
+                            int k = b[i + i1][j + j1] - '0';
+                            v3[k - 1] -= 1;
+                            if (v3[k - 1] < 0)
+                                return false;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
     }
 };
